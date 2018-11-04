4a843205ee5062bdad6e03ba40d672560d9fc15e
Daniel Derzsi <daniel@tohka.us>
2018-11-04 19:19:17 +0200

    final: Final

--- NetworkSimulation/MainMenu.Designer.cs
+++ NetworkSimulation/MainMenu.Designer.cs
@@ -29,32 +29,48 @@
         private void InitializeComponent()
         {
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(347, 175);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(335, 214);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(99, 23);
+            this.button2.Size = new System.Drawing.Size(99, 40);
             this.button2.TabIndex = 1;
-            this.button2.Text = "FloorChooser";
+            this.button2.Text = "ENTER";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LimeGreen;
+            this.label1.Location = new System.Drawing.Point(92, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(602, 73);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Network Simulation";
             // 
             // MainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 369);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Name = "MainMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "MainMenu";
+            this.Text = "Network Simulation";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
     }
 }
