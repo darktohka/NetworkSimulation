4ed8dc14c2f6d4462bb68e1336538a02ed63bee8
Ether3alInfinity <bill.nguyen.250@gmail.com>
2018-11-04 06:09:38 -0600

    Running FloorChooser

---
+++ NetworkSimulation/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetworkSimulation
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FloorChooser fc = new FloorChooser();
+        }
+    }
+}
