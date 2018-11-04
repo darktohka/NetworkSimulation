6bcb82039b19db628c3e70dc3b422c82616cc4c4
KJSony <36554649+KJSony@users.noreply.github.com>
2018-11-04 04:34:54 -0600

    Made commit

---
+++ NetworkSimulation/Connections.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkSimulation
+{
+    public enum ConnectionState
+    {
+        CONNECTED_CABLE = 0,
+        CONNECTED_WIFI = 1,
+        DISCONNECTED = 2
+    }
+}
