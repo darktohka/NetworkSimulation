a2469401d352872f764ed62abad292b71314985f
Ether3alInfinity <bill.nguyen.250@gmail.com>
2018-11-04 02:34:15 -0600

    Update NetworkSimulation/Action.cs

--- NetworkSimulation/Action.cs
+++ NetworkSimulation/Action.cs
@@ -4,7 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Action
+namespace NetworkSimulation
 {
-
+    public class Action
+    {
+        private string Name;
+        private int DeltaUp, DeltaDown;
+        public Action(int dup, ddown)
+        {
+            DeltaUp = dup;
+            DeltaDown = DeltaDown;
+        }
+    }
 }
